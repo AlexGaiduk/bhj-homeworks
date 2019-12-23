218e57855eadd2c36fb60c06a6a6980fbee663fd
alex gaiduk <alex.gaiduk2109@mail.ru>
2019-12-23 20:23:56 +0300

    hm popups

---
+++ element-search/popups/task.js
@@ -0,0 +1,21 @@
+const modalPopups = document.querySelector('#modal_main');
+modalPopups.className = 'modal modal_active';
+
+const modalClose = document.querySelectorAll('div.modal__close');
+
+const modalBtn = document.querySelector('.show-success');
+
+const success = document.querySelector('#modal_success');
+
+close[0].onclick = function () {
+    modalPopups.className = 'modal';
+};
+
+modalBtn.onclick = function () {
+    success.className = 'modal modal_active';
+};
+
+modalClose[1].onclick = function () {
+    modalPopups.className = 'modal';
+    success.className = 'modal';
+};
