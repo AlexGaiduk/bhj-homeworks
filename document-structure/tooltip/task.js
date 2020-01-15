2e3b7b50c77ad1ed3f44a029f43590dbc90fe907
alex gaiduk <alex.gaiduk2109@mail.ru>
2020-01-15 19:25:06 +0300

    hm tooltip

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,24 @@
+
+const tooltip = document.querySelector('.tooltip');
+const links = document.querySelectorAll('.has-tooltip');
+
+for (let link of links) {
+    link.addEventListener('click', showHint);
+}
+
+tooltip.addEventListener('click', () => tooltip.classList.remove('tooltip_active'));
+
+function showHint(event) {
+    event.preventDefault();
+
+    const tooltip = document.querySelector('.tooltip_active');
+
+    if (tooltip) {
+        tooltip.classList.remove('tooltip_active');
+    }
+
+    tooltip.classList.add('tooltip_active');
+    tooltip.innerText = event.target.title;
+    tooltip.style.left = event.target.getBoundingClientRect().left + 'px';
+    tooltip.style.top = event.target.getBoundingClientRect().top + 20 + 'px';
+}
